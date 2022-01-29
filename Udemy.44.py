1e83531bb0b9da68cfd7bb1fc3fc19980f35283c
Nagi <nagisa1755@gmail.com>
2022-01-29 15:53:25 +0900

    range関数。

---
+++ Udemy.44.py
@@ -0,0 +1,12 @@
+# num_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# for i in num_list:
+#   print(i)
+#for i in range(2, 10):
+#   print(i)
+
+#10回分ループを何かしたい場合
+for i in range(10):
+    print(i, 'hello')
+#iを使わない場合、アンダースコアをいれる。
+
+
