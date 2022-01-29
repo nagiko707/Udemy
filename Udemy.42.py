9a1dd71a4957161a94231c475981623e4ddef881
Nagi <nagisa1755@gmail.com>
2022-01-29 14:00:50 +0900

    for文とbreak文とcontinue文。

---
+++ Udemy.42.py
@@ -0,0 +1,24 @@
+some_list = [1, 2, 3, 4, 5]
+
+'''
+i = 0
+while i < len(some_list):
+    print(some_list[i])
+    i += 1
+'''
+
+'''
+for i in some_list:
+    print(i)
+'''
+
+'''
+for s in 'abcde':
+    print(s)
+'''
+
+for word in ['My', 'name', 'is', 'Mike']:
+    if word == 'name':
+        continue
+        #break
+    print(word)
